0c55864bf9e28c9087d2ebfee5b41044cde6a1a4
aaazzz <akrm@hey.com>
2024-10-25 18:02:11 +0700

    update

--- dbt_flow.py
+++ dbt_flow.py
@@ -3,18 +3,30 @@ from prefect import flow, task
 from prefect_dbt import DbtCoreOperation
 
 @task
-def run_dbt():
-    dbt_op = DbtCoreOperation.load("dbt-code-operation")
-    result = dbt_op.run()
+def run_dbt(use_block: bool = False):
+    if use_block:
+        dbt_op = DbtCoreOperation.load("dbt-code-operation")
+        result = dbt_op.run()
+
+    else:
+        # ローカル実行時
+        dbt_op = DbtCoreOperation(
+            commands=["pwd", "dbt run"],
+            project_dir="./flow/dbt/dbt_project",
+            profiles_dir="./flow/dbt"
+        )
+        result = dbt_op.run()
     return result
 
+
 @flow
-def my_flow():
-    run_dbt()
+def my_flow(use_block: bool = False):
+    run_dbt(use_block=use_block)
 
 
 if __name__ == "__main__":
     my_flow.serve(
         name="run-dbt-on-local",
-        interval=60
+        interval=60,
+        parameters={"use_block": False}
      )
