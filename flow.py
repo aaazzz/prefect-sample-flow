7addc17839d31082c5c51a693b30ac9c05e91ae9
aaazzz <akrm@hey.com>
2024-10-25 12:00:27 +0700

    connect to motherduck

--- flow.py
+++ flow.py
@@ -3,7 +3,8 @@ import duckdb
 
 @flow(log_prints=True)
 def my_flow():
-    df = duckdb.sql("SELECT 42")
+    con = duckdb.connect("md:?motherduck_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFraXJhQHNhbmdvLXRlY2guY29tIiwic2Vzc2lvbiI6ImFraXJhLnNhbmdvLXRlY2guY29tIiwicGF0IjoiN28wZmdtTEo3REFqV0VnWjV4TVVwUGhUREdhMVRZaGRiMUpYeExGYW9vSSIsInVzZXJJZCI6Ijc2ZmQ2YTFlLWE2NGEtNDhhNi1iMGE0LWI4MjFlODRmZWQyOCIsImlzcyI6Im1kX3BhdCIsImlhdCI6MTcyOTgyOTk1NH0.Z1FO2MLAiYD0uNg5aVOu75NyIDy5ZbfM8Tw9yGBxxRw")
+    df = con.sql("SELECT COUNT(*) FROM orders2")
     print(df)
 
 
