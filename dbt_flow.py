b088f35e2c424ffe84fbbc09e67cfccea335aba4
aaazzz <akrm@hey.com>
2024-10-26 10:28:30 +0700

    update

--- dbt_flow.py
+++ dbt_flow.py
@@ -5,28 +5,34 @@ from prefect_dbt import DbtCoreOperation
 @task
 def run_dbt(use_block: bool = False):
     if use_block:
+        # Remote
         dbt_op = DbtCoreOperation.load("dbt-code-operation")
         result = dbt_op.run()
 
     else:
-        # ローカル実行時
+        # Local
         dbt_op = DbtCoreOperation(
-            commands=["pwd", "dbt run"],
+            commands=["dbt run"],
             project_dir="./flow/dbt/dbt_project",
             profiles_dir="./flow/dbt"
         )
         result = dbt_op.run()
     return result
 
+@task
+def get_pwd():
+        print ("Current directory:" , os.getcwd())
+
 
 @flow
 def my_flow(use_block: bool = False):
+    get_pwd()
     run_dbt(use_block=use_block)
 
 
 if __name__ == "__main__":
     my_flow.serve(
         name="run-dbt-on-local",
-        interval=60,
+        interval=10,
         parameters={"use_block": False}
      )
