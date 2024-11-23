b0597354380521e125c1322258306955570a01e0
aaazzz <akrm@hey.com>
2024-11-23 17:31:33 +0700

    change table name

--- dbt/dbt_project/models/warehouse/goods.sql
+++ dbt/dbt_project/models/warehouse/goods.sql
@@ -15,7 +15,7 @@ WITH deduplicated_goods AS (
              -- ORDER BY row_id DESC
         ) AS row_num
     FROM
-        staging_goods  -- データレイクから取り込んだ重複が含まれるデータ
+        goodscsv  -- データレイクから取り込んだ重複が含まれるデータ
 )
 
 -- row_num = 1（最新のデータのみ）を抽出
