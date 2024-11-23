b7dfd8b74137a12f578f04ac57e4216e5d3bf545
aaazzz <akrm@hey.com>
2024-11-23 18:10:36 +0700

    add a param to sample flow

--- flow.py
+++ flow.py
@@ -1,8 +1,12 @@
 from prefect import flow
+from typing import Optional
+
 
 @flow(log_prints=True)
-def my_flow():
-    print('hello!')
+def my_flow(date: Optional[datetime] = None):
+    if date is None:
+        date = datetime.now(timezone.utc)
+    print(f"It was {date.strftime('%A')} on {date.isoformat()}")
 
 
 if __name__ == "__main__":
